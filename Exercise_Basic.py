a12eb3caa0c174594f72703e31a33867d5f79a7e
Nikhil <nikhil.rajendrakumar@gmail.com>
2019-06-02 16:02:54 -0400

    Beginning to use elif

---
+++ Exercise_Basic.py
@@ -0,0 +1,13 @@
+
+import pandas as pd
+import numpy as np
+
+for item in range(0,120)
+    if item % 3 == 1 and item % 5 == 3
+        print('Cracked')
+    elif item % 3 == 1
+        print('Half Cracked')
+    elif item % 5 == 0
+        print('High5')
+    else :
+        print(item)
