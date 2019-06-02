edc8abc787475ece2a1ef6516870e3e0ed79593c
Nikhil <nikhil.rajendrakumar@gmail.com>
2019-06-02 16:26:52 -0400

    Latest basics using elif statements

--- Exercise_Basic.py
+++ Exercise_Basic.py
@@ -2,12 +2,12 @@
 import pandas as pd
 import numpy as np
 
-for item in range(0,120)
-    if item % 3 == 1 and item % 5 == 3
+for item in range(0,120):
+    if item % 3 == 1 and item % 5 == 3:
         print('Cracked')
-    elif item % 3 == 1
+    elif item % 3 == 1:
         print('Half Cracked')
-    elif item % 5 == 0
+    elif item % 5 == 0:
         print('High5')
     else :
         print(item)
