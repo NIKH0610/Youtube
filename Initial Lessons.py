843ade3405acf3151fa45c7fa663d6f62d8c72de
Nikhil <nikhil.rajendrakumar@gmail.com>
2019-06-02 16:59:49 -0400

    Very Basic stuff

---
+++ Initial Lessons.py
@@ -0,0 +1,18 @@
+#name = input('What is your name? ')
+#print('Hi ' + name)
+#color = input('What is your favourite color? ')
+#print(name + ' likes ' + color)
+
+#Birth_Year = input('Year of Birth: ')
+#print(type(Birth_Year))
+#age = 2019 - int(Birth_Year)
+#print(type(age))
+#print(age)
+
+#Weight = input('Enter weight in pounds: ')
+#weight_pounds = int(Weight) / 2.205
+#print(weight_pounds)
+
+course = "Python for beginners"
+print(course[-1])
+
